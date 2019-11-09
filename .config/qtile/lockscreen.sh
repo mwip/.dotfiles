110d5cea71ff134cdfe290b632242135177f7138
mwip <matthias.weigand@protonmail.com>
2019-11-10 00:11:49 +0100

    improved pixelation and removing tmp image with screenshots

--- .config/qtile/lockscreen.sh
+++ .config/qtile/lockscreen.sh
@@ -2,6 +2,7 @@
 ICON=$HOME/.config/qtile/icon.png
 TMPBG=/tmp/screen.png
 scrot $TMPBG
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG -scale 5% -scale 2000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -u -i $TMPBG
+sleep 5 & rm $TMPBG

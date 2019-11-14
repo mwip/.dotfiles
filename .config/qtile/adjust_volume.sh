d0eebc4a9b64218e3eae906817cafc96d3744628
mwip <matthias.weigand@protonmail.com>
2019-11-14 10:08:29 +0100

    improving adjust_volume.sh

--- .config/qtile/adjust_volume.sh
+++ .config/qtile/adjust_volume.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PERCENT=5
+MAXVOLUME=200
 
 # pactl version
 # pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo $1$PERCENT% && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
@@ -8,15 +9,18 @@ PERCENT=5
 # pamixer version
 if [ $1 == '+' ]
 then
-   pamixer -i $PERCENT && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+    if [ $(pamixer --get-volume) -lt "$MAXVOLUME" ]
+       then
+	   pamixer -i $PERCENT --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+    fi
 fi
 
 if [ $1 == '-' ]
 then
-   pamixer -d $PERCENT && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+   pamixer -d $PERCENT --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
 fi    
    
 if [ $1 == 'm' ]
 then
-    pamixer -t && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+    pamixer -t --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
 fi

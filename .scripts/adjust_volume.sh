d154bf8499bed5a275f8b4685a7005de90b4d927
mwip <matthias.weigand@protonmail.com>
2019-11-18 09:01:51 +0100

    fixing and improving adjust_volume.sh

--- .scripts/adjust_volume.sh
+++ .scripts/adjust_volume.sh
@@ -2,6 +2,7 @@
 
 PERCENT=5
 MAXVOLUME=200
+SOUND=.scripts/audio-volume-change.oga
 
 # pactl version
 # pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo $1$PERCENT% && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
@@ -11,16 +12,16 @@ if [ $1 == '+' ]
 then
     if [ $(pamixer --get-volume) -lt "$MAXVOLUME" ]
        then
-	   pamixer -i $PERCENT --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+	   pamixer -i $PERCENT --allow-boost && gst-play-1.0 $SOUND
     fi
 fi
 
 if [ $1 == '-' ]
 then
-   pamixer -d $PERCENT --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+   pamixer -d $PERCENT --allow-boost && gst-play-1.0 $SOUND
 fi    
    
 if [ $1 == 'm' ]
 then
-    pamixer -t --allow-boost && gst-play-1.0 $HOME/.config/qtile/audio-volume-change.oga
+    pamixer -t --allow-boost && gst-play-1.0 $SOUND
 fi

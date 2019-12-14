a0c126f96b7ac7453217f7bbe8689dc8430761cf
mwip <matthias.weigand@protonmail.com>
2019-12-14 14:18:38 +0100

    adding dwm_bar.sh

---
+++ .scripts/dwm_bar.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+dte(){
+  dte="$(date +"%Y-%m-%d %H:%M")"
+  echo -e "$dte"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "$mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "$cpu% cpu"
+}
+
+while true; do
+     xsetroot -name "$(cpu) | $(mem) | $(dte)"
+     sleep 10s    # Update time every ten seconds
+done &

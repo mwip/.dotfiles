2763adc1b68cf73f6e97d7f2b915746696945b6f
mwip <matthias.weigand@protonmail.com>
2020-01-05 12:40:42 +0100

    adding dwm support for jabref

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -2,6 +2,7 @@
 
 # xrandr --output VGA-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output VGA-2 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA-3 --mode 1200x1600 --pos 3840x0 --rotate normal
 #xrandr --output VGA-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output VGA-2 --mode 1920x1200 --pos 0x0 --rotate normal
+wmname LG3D & # DWM tweak to get jabref running
 compton &
 nitrogen --restore & 
 urxvtd -q -o -f &
@@ -27,7 +28,7 @@ fi
 if [ $(hostname) == "walhall" ]
 then
     # start mpd notifications (using dunst)
-    mpDris2 &
+    sleep 30 & mpDris2 &
 fi
 synology-drive &
 

197ee7713821bdd418b1ac2a258be73b8c0b4b2b
mwip <matthias.weigand@protonmail.com>
2020-03-20 08:35:06 +0100

    Rearrange the autostart script

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -3,7 +3,7 @@
 # Device specific autostarts 
 if [ $(hostname) == "walhall" ]
 then
-    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI-0 --off --output DVI-0 --off --output DVI-1 --primary --mode 1920x1200 --pos 1680x0 --rotate normal
+    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI-0 --off --output DVI-0 --off --output DVI-1 --primary --mode 1920x1200 --pos 1680x0 --rotate normal &
 fi &
 xbindkeys &
 wmname LG3D & # DWM tweak to get jabref running
@@ -16,8 +16,6 @@ xsetroot -cursor_name left_ptr &
 keepassxc &
 xrdb -merge ~/.Xresources &
 nm-applet &
-setxkbmap de &
-setxkbmap -option ctrl:nocaps & # remap capslock to control
 syncthing-gtk &
 dunst &
 redshift-gtk -t 6500:3000 -l 48.13:11.57 & 
@@ -40,6 +38,8 @@ esac && $HOME/.scripts/net_speed.sh $interface &
 # Device specific autostarts 
 if [ $(hostname) == "bifrost" ]
 then
+    setxkbmap de &
+    setxkbmap -option ctrl:nocaps & # remap capslock to control
     # remap lenovo print key to context menu key
     xmodmap -e "keycode 107 = Menu" &
     # reduce size in alacritty 
@@ -48,3 +48,4 @@ then
     # sleep 30 & mpDris2 & # should be started as systemd service
 fi &
 emacs --daemon --chdir=$HOME &
+sleep 5 && $HOME/.scripts/restart_systray.sh stalonetray &

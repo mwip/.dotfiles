fe882c9c3bbfb20f6f524fea1a3fc940911755ce
mwip <matthias.weigand@protonmail.com>
2020-04-04 11:13:00 +0200

    Scripts: Add all choices to dmenu_displayselect.sh

--- .scripts/dmenu_displayselect.sh
+++ .scripts/dmenu_displayselect.sh
@@ -4,14 +4,13 @@ CHOICES="Laptop\nDual\nHDMI-only\nManual Selection"
 
 CHOSEN=$(echo -e "$CHOICES" | dmenu -i)
 
-
-
 case "$CHOSEN" in
     Laptop) xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output VIRTUAL1 --off;;
     Dual) xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off;;
-    HDMI-only) ;;
-    "Manual Selection") ;;
+    HDMI-only) xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off;;
+    "Manual Selection") arandr;;
 esac
 
 # restart WM
-qtile-cmd -o cmd -f restart
+# qtile-cmd -o cmd -f restart
+xmonad --restart && sleep 3 && $HOME/.scripts/restart_systray.sh stalonetray &

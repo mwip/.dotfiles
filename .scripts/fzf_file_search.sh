3b51d9d7a29acebd911e9c1bdee035da2215ab2e
mwip <matthias.weigand@protonmail.com>
2020-04-12 00:07:47 +0200

    fzf_file_search: add alias and improve emacs hanling

--- .scripts/fzf_file_search.sh
+++ .scripts/fzf_file_search.sh
@@ -2,6 +2,16 @@
 
 file=$(find . -maxdepth 4 | fzf --preview='pistol {}')
 
-prgrm=$(echo -e "emacsclient\nvim\nless\ncat" | dmenu -i -p "Open with: ")
+prgrm=$(echo -e "emacsclient\nvim\nless\ncat\nxclip" | dmenu -i -p "Open with: " | sed "s/emacsclient/emacsclient -c -n -a ''/")
 
-$prgrm $file
+case $prgrm in
+    xclip)
+	echo "$file" | xclip -r
+	;;
+    "")
+	echo "$file"
+      ;;
+    *)
+	$prgrm $file
+	;;
+esac

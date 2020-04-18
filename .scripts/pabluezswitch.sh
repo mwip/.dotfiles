78fe392643b0e13d4bb6b97bafc1e88cfa812298
mwip <matthias.weigand@protonmail.com>
2020-04-18 17:32:40 +0200

    pabluezswitch: add notify-send and icon

--- .scripts/pabluezswitch.sh
+++ .scripts/pabluezswitch.sh
@@ -4,3 +4,4 @@ BLUEZCARD=`pactl list cards short | egrep -o bluez.*[[:space:]]`
 pactl set-card-profile $BLUEZCARD a2dp_sink
 pactl set-card-profile $BLUEZCARD headset_head_unit
 pactl set-card-profile $BLUEZCARD a2dp_sink
+notify-send -i $HOME/.scripts/icons/bluetooth_headphones.png "Bluetooth Headset" "Audio profile reset"

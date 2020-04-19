99265c143f716572bdbcf4df20438ed27cf704c0
mwip <matthias.weigand@protonmail.com>
2020-04-19 18:15:35 +0200

    Dmenu_displayselect: add color and fix resart
    previously the WM was restarted even if nothing had changed. This patch
    fixes this. Also a new color is added to fancy up the dmenu prompt

--- .scripts/dmenu_displayselect.sh
+++ .scripts/dmenu_displayselect.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 CHOICES="Laptop\nDual\nHDMI-only\nManual Selection\n~~edit~~"
+DMENU_CMD='dmenu -i -fn "Ubuntu Mono:size=11" -nb "#161616" -nf "#D0D0D0" -sf "#444444" -sb "#479E61"'
+CHOSEN=$(echo -e "$CHOICES" | eval "$DMENU_CMD -p 'Select the xrandr preset:'")
 
-CHOSEN=$(echo -e "$CHOICES" | dmenu -i)
-
-case "$CHOSEN" in
-    Laptop)
-	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output VIRTUAL1 --off
-	;;
-    Dual)
-	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
-	;;
-    HDMI-only)
-	xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-	;;
-    "Manual Selection")
-	arandr
-	;;
-    "~~edit~~")
-	$MYTERM vim ${BASH_SOURCE[0]}
-esac
-
-# restart WM
-# qtile-cmd -o cmd -f restart
-xmonad --restart && sleep 3 && $HOME/.scripts/restart_systray.sh stalonetray &
+[ -n "$CHOSEN" ] &&
+    case "$CHOSEN" in
+	Laptop)
+	    xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output VIRTUAL1 --off
+	    ;;
+	Dual)
+	    xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
+	    ;;
+	HDMI-only)
+	    xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+	    ;;
+	"Manual Selection")
+	    arandr
+	    ;;
+	"~~edit~~")
+	    $MYTERM vim ${BASH_SOURCE[0]}
+esac &&
+    xmonad --restart && sleep 3 && $HOME/.scripts/restart_systray.sh stalonetray &

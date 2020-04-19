d259f8b665cd6a8784f6a0296c0eac2ed2541651
mwip <matthias.weigand@protonmail.com>
2020-04-19 09:07:15 +0200

    Dmenu_filemanager: change dmenu colors

--- .scripts/dmenu_filemanager.sh
+++ .scripts/dmenu_filemanager.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-DMENU="/usr/bin/dmenu -i -p"
+DMENU_CMD='dmenu -i -fn "Ubuntu Mono:size=11" -nb "#161616" -nf "#D0D0D0" -sf "#444444" -sb "#82AAFF"'
 FOLDERS="home\nCloudStation\nDownloads/\nDocuments/\n.config\n~~edit~~"
 
-CHOICE=$(echo -e $FOLDERS | $DMENU "Launch file browser at:" )
+CHOICE=$(echo -e $FOLDERS | eval "$DMENU_CMD -p 'Launch $(echo $MYFILEMNGR | awk '{print $1}') at:'" )
 
-case $CHOICE in
-    home)
-	$MYFILEMNGR $HOME
-	;;
-    '~~edit~~')
-	$MYTERM vim ${BASH_SOURCE[0]}
-	;;
-    *)
-	doublecmd -T $CHOICE
-esac
-
-
+[ -n "$CHOICE" ] &&
+    case $CHOICE in
+	home)
+	    $MYFILEMNGR $HOME
+	    ;;
+	'~~edit~~')
+	    $MYTERM vim ${BASH_SOURCE[0]}
+	    ;;
+	*)
+	    doublecmd -T $CHOICE
+    esac

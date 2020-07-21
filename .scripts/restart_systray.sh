b74dd533b6e35973bb54ed31013d78036b0936ae
Matthias Weigand <matthias.weigand@protonmail.com>
2020-07-21 22:11:28 +0200

    Scripts: improve xdotool in restart_systray.sh

--- .scripts/restart_systray.sh
+++ .scripts/restart_systray.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
 if [ $1 == "stalonetray" ]; then
+    # raise the window to top
     xdotool search --class stalonetray windowraise
+    # move the window to the outer right corner minus its own x-size
+    xdotool search --class stalonetray windowmove \
+	    $(( 1920 * 2 - \
+	    $(xdotool search --class stalonetray getwindowgeometry | \
+	      grep Geometry | sed 's/  Geometry: //;s/x.*//') )) y
 fi

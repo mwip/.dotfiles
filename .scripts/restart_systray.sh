f553480765150f3b26576a1d0243a6e059947b5d
mwip <matthias.weigand@protonmail.com>
2020-07-23 19:14:06 +0200

    Scripts: fix restart_systray.sh for smaller display

--- .scripts/restart_systray.sh
+++ .scripts/restart_systray.sh
@@ -3,9 +3,19 @@
 if [ $1 == "stalonetray" ]; then
     # raise the window to top
     xdotool search --class stalonetray windowraise
+    # set multiplyer for different displays using hostnames
+    case $(hostname) in 
+	    andlang) 
+		    multiplyer=2
+		    ;;
+	    *)
+		    multiplyer=1
+		    ;;
+    esac
     # move the window to the outer right corner minus its own x-size
     xdotool search --class stalonetray windowmove \
-	    $(( 1920 * 2 - \
+	    $(( 1920 * $multiplyer - \
 	    $(xdotool search --class stalonetray getwindowgeometry | \
 	      grep Geometry | sed 's/  Geometry: //;s/x.*//') )) y
 fi
+

1c304625e9d0da4ff772b65c60b1a6b78241fccf
Matthias Weigand <matthias.weigand@protonmail.com>
2020-08-23 21:52:12 +0200

    Emacs: improve evil

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,11 +34,10 @@
      ("" "amssymb" t nil)
      ("" "capt-of" nil nil)
      ("colorlinks=true, allcolors=MidnightBlue, final=true" "hyperref" nil nil)
-     ("dvipsnames, usenames" "xcolor" nil nil))))
+     ("dvipsnames, usenames" "xcolor" nil nil)))
  '(package-selected-packages
-   (quote
-    (transpose-frame mu4e-alert mu4e evil rainbow-delimiters ido-vertical-mode dockerfile-mode openwith org-pdftools syntax-subword org-ref fill-column-indicator company-distel company-jedi magit git-gutter neotree all-the-icons-ivy treemacs doom-themes doom-modeline dumb-jump ag counsel-projectile projectile powerline iedit expand-region undo-tree multiple-cursors yasnippet-snippets yasnippet flycheck ess org which-key use-package try tabbar org-bullets counsel color-theme-modern auto-org-md auto-complete ace-window)))
- '(send-mail-function (quote smtpmail-send-it)))
+   '(transpose-frame mu4e-alert mu4e evil rainbow-delimiters ido-vertical-mode dockerfile-mode openwith org-pdftools syntax-subword org-ref fill-column-indicator company-distel company-jedi magit git-gutter neotree all-the-icons-ivy treemacs doom-themes doom-modeline dumb-jump ag counsel-projectile projectile powerline iedit expand-region undo-tree multiple-cursors yasnippet-snippets yasnippet flycheck ess org which-key use-package try tabbar org-bullets counsel color-theme-modern auto-org-md auto-complete ace-window))
+ '(send-mail-function 'smtpmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

3740bc5490e0d0480d811d307ef39cef896b39d0
mwip <matthias.weigand@protonmail.com>
2020-11-15 16:43:53 +0100

    Emacs: more evil-leader

--- .scripts/dmenu_filemanager.sh
+++ .scripts/dmenu_filemanager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DMENU_CMD='dmenu -i -fn "Ubuntu Mono:size=11" -nb "#161616" -nf "#D0D0D0" -sf "#444444" -sb "#82AAFF"'
-FOLDERS="home\nNextCloud\nDownloads/\nDocuments/\n.config\n.scripts\n~~edit~~"
+FOLDERS="home\nNextcloud\nDownloads/\nDocuments/\n.config\n.scripts\n~~edit~~"
 
 CHOICE=$(echo -e $FOLDERS | eval "$DMENU_CMD -p 'Launch $(echo $MYFILEMNGR | awk '{print $1}') at:'" )
 

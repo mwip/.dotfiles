a7c736826c414f06498458e202adecfc0656b71a
mwip <matthias.weigand@protonmail.com>
2020-12-06 20:39:31 +0100

    Improve copernicus_wallpaper.sh

--- .scripts/coperincus_wallpaper.sh
+++ .scripts/coperincus_wallpaper.sh
@@ -6,9 +6,10 @@ mkdir -p ~/.cache/wp/
 
 URL="https://esamultimedia.esa.int/multimedia/eo/calendar2020/2020_Sentinels_digital_calendar_1920x1080_$(date +%m).jpg"
 
-while [ ! -f $CACHE_FILE ];
+until wget $URL -O $CACHE_FILE &> /dev/null
 do
-    wget $URL -O $CACHE_FILE
+	echo "No success... Wait and retry"
+        sleep 5
 done
 
 BG_COL=$(convert $CACHE_FILE -resize 1x1 txt:- | grep -Po "#[[:xdigit:]]{6}")

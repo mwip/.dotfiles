b57555dabf94022c9c239762ee20cacf9fd7c7cc
Matthias Weigand <matthias.weigand@protonmail.com>
2021-01-22 20:16:50 +0100

    Fix Copernicus wallpaper

--- .scripts/coperincus_wallpaper.sh
+++ .scripts/coperincus_wallpaper.sh
@@ -12,7 +12,7 @@ mkdir -p ~/.cache/wp/
 CACHE_FILE=~/.cache/wp/sentinel_calendar_$YEAR$MNTH.jpg
 URL="https://esamultimedia.esa.int/multimedia/eo/calendar"$YEAR"/"$YEAR"_Sentinels_digital_calendar_FB_TW%20feed_1920x1080_$MNTH.jpg"
 
-if [ -f $CACHE_FILE ]; then
+if [ ! -f $CACHE_FILE ]; then
     until wget $URL -O $CACHE_FILE &> /dev/null
     do
 	echo "No success... Wait and retry"

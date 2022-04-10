7af3ee781bfad2ad89de0ce47394643824caf293
mwip <matthias.weigand@protonmail.com>
2022-04-10 16:00:43 +0200

    Qtile: further tweak config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,12 +1,17 @@
+import socket
 from os.path import expanduser
 from subprocess import run
 from copy import deepcopy
-from libqtile import bar, layout, widget, hook
-from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
+from libqtile import bar, layout, widget, hook, extension
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown, KeyChord
 from libqtile.lazy import lazy
 
 MOD = "mod4"
-terminal = "alacritty"
+TERMINAL = "alacritty"
+MYFONT = "Hack Nerd Font Mono"
+HOST = socket.gethostname()
+HOME = "/home/loki"
+SCRIPTS = f"{HOME}/.scripts"
 
 
 @hook.subscribe.startup_once
@@ -18,11 +23,11 @@ keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([MOD], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([MOD], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([MOD], "j", lazy.layout.down(), desc="Move focus down"),
     Key([MOD], "k", lazy.layout.up(), desc="Move focus up"),
     Key([MOD], "t", lazy.window.toggle_floating()),
+    Key([MOD], "b", lazy.hide_show_bar()),
+    Key([MOD], "w", lazy.spawn("rofi -show window -show-icons -matching fuzzy")),
     Key([MOD], "q", lazy.window.kill()),
     # Toggle between different layouts as defined below
     Key([MOD], "space", lazy.next_layout(), desc="Toggle between layouts"),
@@ -56,60 +61,104 @@ keys = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([MOD], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([MOD], "Return", lazy.spawn(TERMINAL), desc="Launch terminal"),
     Key([MOD, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([MOD, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     # spawn applications
     Key(
         [MOD],
         "r",
-        lazy.spawn(
-            "dmenu_run -i -fn 'Hack Nerd Font Mono:size=16' -nb '#161616' -nf '#D0D0D0' -sf '#444444' -sb '#C45500'"
+        lazy.run_extension(
+            extension.DmenuRun(
+                dmenu_prompt=">>>",
+                dmenu_command="dmenu_recent.sh",
+                dmenu_font=MYFONT,
+                # dmenu_height=16,
+                dmenu_ignorecase=True,
+                foreground="#D0D0D0",
+                background="#242730",
+                selected_foreground="#444444",
+                selected_background="#C45500",
+            )
         ),
         desc="Spawn a command using a prompt widget",
     ),
     Key([MOD], "f", lazy.spawn("librewolf"), desc="Launch Firefox"),
+    # Emacs KeyChords
+    KeyChord(
+        [MOD],
+        "e",
+        [
+            Key([], "e", lazy.spawn("emacsclient -c -a 'emacs'"), desc="Launch Emacs"),
+            Key([], "m", lazy.spawn("emacsclient -c -a 'emacs' -e '(mu4e)'"), desc="Launch mu4e"),
+            Key(
+                [],
+                "c",
+                lazy.spawn("emacsclient -c -a 'emacs' -e '(org-capture)'"),
+                desc="Launch org capture",
+            ),
+            Key([], "q", lazy.spawn(f"{SCRIPTS}/restart_emacs.sh")),
+        ],
+    ),
     # Media controls
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +5%")),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -5%")),
+    Key(
+        [],
+        "XF86AudioRaiseVolume",
+        lazy.spawn("pactl set-sink-volume 0 +5%"),
+        lazy.spawn("mpv /tmp/avc.wav"),
+    ),
+    Key(
+        [],
+        "XF86AudioLowerVolume",
+        lazy.spawn("pactl set-sink-volume 0 -5%"),
+        lazy.spawn("mpv /tmp/avc.wav"),
+    ),
+    Key(
+        [],
+        "XF86AudioMute",
+        lazy.spawn("pactl set-sink-mute 0 toggle"),
+        lazy.spawn("mpv /tmp/avc.wav"),
+    ),
     Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight + 10 -time 0")),
+    Key([MOD, "shift"], "c", lazy.spawn(f"{SCRIPTS}/xcolor_pick.sh")),
     Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight - 10 -time 0")),
-    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute 0 toggle")),
-    Key([], "F11", lazy.group["scratchpad"].dropdown_toggle("qalc")),
-    Key([], "F12", lazy.group["scratchpad"].dropdown_toggle("cal")),
+    # scratchpads
+    Key([MOD], "F11", lazy.group["scratchpad"].dropdown_toggle("qalc")),
+    Key([MOD], "F12", lazy.group["scratchpad"].dropdown_toggle("cal")),
 ]
 
 groups = [
-    Group("1", label="1:", layout="max", matches=Match(wm_class=["Firefox"])),
-    Group("2", label="2:", layout="max", matches=Match(title=["emacs@*"])),
+    Group("1", label="1:", layout="max", matches=[Match(wm_class=["librewolf"])]),
+    Group("2", label="2:", layout="max", matches=[Match(wm_class=["Emacs"])]),
     Group("3", label="3:", layout="monadtall"),
     Group("4", label="4:", layout="max"),
     Group("5", label="5:", layout="max"),
     Group("6", label="6:ﱘ", layout="monadtall"),
     Group("7", label="7:", layout="floating"),
-    Group("8", label="8:"),
-    Group("9", label="9:"),
-    Group("0", label="0:"),
+    Group("8", label="8:", layout="max"),
+    Group("9", label="9:", layout="max"),
+    Group("0", label="0:", layout="monadtall"),
+    Group("ssharp", label="ß:", layout="floating"),
 ]
 
 # groups = [Group(i) for i in "123456789"]
 
-for i in groups:
+for group in groups:
     keys.extend(
         [
             # mod1 + letter of group = switch to group
             Key(
                 [MOD],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                group.name,
+                lazy.group[group.name].toscreen(),
+                desc="Switch to group {}".format(group.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [MOD, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=False),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                group.name,
+                lazy.window.togroup(group.name, switch_group=False),
+                desc="Switch to & move focused window to group {}".format(group.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
@@ -146,70 +195,99 @@ layouts = [
     layout.MonadTall(margin=6),
     layout.Max(),
     layout.Floating(),
-    layout.Matrix(margin=6, border_focus="#ff0000", border_width=2),
 ]
 
 widget_defaults = dict(
-    font="Hack Nerd Font Mono",
+    font=MYFONT,
     fontsize=14,
+    foreground="#bbc2cf",
+    fgcolor_normal="#bbc2cf",
     padding=5,
 )
 
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        top=bar.Bar(
-            widgets=[
-                widget.GroupBox(disable_drag=True, highlight_method="line"),
-                widget.Sep(),
-                widget.CurrentLayout(),
-                widget.Sep(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Sep(),
-                widget.Net(),
-                widget.Sep(),
-                widget.ThermalZone(),
-                widget.Sep(),
-                widget.BatteryIcon(),
-                widget.Battery(format="{char} {percent:2.0%} {hour:d}:{min:02d}h"),
-                widget.Sep(),
-                widget.PulseVolume(
-                    fmt="Vol: {}",
-                    get_volume_command="pulsemixer --get-volume | awk '{print $1}'",
-                ),
-                widget.Sep(),
-                widget.Backlight(
-                    brightness_file="/sys/class/backlight/intel_backlight/brightness",
-                    max_brightness_file="/sys/class/backlight/intel_backlight/max_brightness",
-                ),
-                widget.Sep(),
-                widget.Clock(format="%Y-%m-%d %a %H:%M"),
-                widget.Sep(),
-                widget.QuickExit(default_text="[shutdown]", countdown_format="[{} seconds]"),
-                widget.Sep(),
-                widget.Systray(),
-            ],
-            size=24,  # height of bar
+mywidgets = []
+# Groups
+mywidgets.extend(
+    [
+        widget.GroupBox(
+            padding=1,
+            font=MYFONT,
+            disable_drag=True,
+            highlight_method="text",
+            highlight_color="#e69055",
+            active="#1f5582",
+            this_current_screen_border="#e69055",
+            this_screen_border="#e69055",
+            other_screen_border="#7bc275",
+            other_current_screen_border="#7bc275",
+        )
+    ]
+)
+# Layout
+mywidgets.extend([widget.Sep(), widget.CurrentLayout()])
+# Window title
+mywidgets.extend([widget.Sep(), widget.WindowName()])
+
+# andlang specific
+if HOST == "andlang":
+    # mpd
+    widget.Mpd2()
+
+# Network usage
+mywidgets.extend([widget.Sep(), widget.Net(format=" {down} ↓↑ {up}"), widget.NetGraph()])
+# Temperature
+mywidgets.extend([widget.Sep(), widget.ThermalZone()])
+
+# bifrost specific
+if HOST == "bifrost":
+    # Battery
+    mywidgets.extend(
+        [
+            widget.Sep(),
+            widget.BatteryIcon(),
+            widget.Battery(format="{char} {percent:2.0%} {hour:d}:{min:02d}h"),
+        ]
+    )
+    # Backlight
+    mywidgets.extend(
+        [
+            widget.Sep(),
+            widget.Backlight(
+                brightness_file="/sys/class/backlight/intel_backlight/brightness",
+                max_brightness_file="/sys/class/backlight/intel_backlight/max_brightness",
+                fmt=" {}",
+            ),
+        ]
+    )
+
+# Audio level
+mywidgets.extend(
+    [
+        widget.Sep(),
+        widget.Volume(
+            fmt="墳 {}",
+            # get_volume_command="pulsemixer --get-volume | awk '{print $1}'",
+            update_interval=0.2,
         ),
-    ),
-]
+    ]
+)
+# Time
+mywidgets.extend([widget.Sep(), widget.Clock(format="%Y-%m-%d %a %H:%M")])
+# Systray
+mywidgets.extend(
+    [
+        widget.Sep(),
+        widget.Systray(padding=0),
+    ]
+)
+
+screens = [Screen(top=bar.Bar(widgets=mywidgets, size=24, background="#242730"))]
 
 # Drag floating layouts.
 mouse = [
-    Drag(
-        [MOD],
-        "Button1",
-        lazy.window.set_position_floating(),
-        start=lazy.window.get_position(),
-    ),
+    Drag([MOD], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([MOD], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([MOD], "Button2", lazy.window.bring_to_front()),
 ]

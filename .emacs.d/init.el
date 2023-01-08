1627e928d3cc234142e3343597603ec688b6c391
Matthias Weigand <matthias.weigand@protonmail.com>
2023-01-08 14:02:11 +0100

    Emacs: purge company tooltips from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,11 +45,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
- ;; '(company-tooltip ((t (:inherit default :background "#27b72b1e2e86"))))
- ;; '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- ;; '(company-tooltip-scrollbar-thumb ((t (:background "#2eca32cc36cf"))))
- ;; '(company-tooltip-scrollbar-track ((t (:background "#3a943f99449e"))))
- ;; '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 (put 'narrow-to-region 'disabled nil)

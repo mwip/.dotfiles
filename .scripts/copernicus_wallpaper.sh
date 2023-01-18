bfad709f6e050861f4db8f0d79add6310d01372c
mwip <matthias.weigand@protonmail.com>
2023-01-18 10:17:00 +0100

    Update copernicus Wallpapers to 2023

--- .scripts/copernicus_wallpaper.sh
+++ .scripts/copernicus_wallpaper.sh
@@ -10,10 +10,10 @@ MNTH=$(date +%m)
 mkdir -p ~/.cache/wp/
 
 CACHE_FILE=~/.cache/wp/sentinel_calendar_$YEAR$MNTH.jpg
-URL="https://esamultimedia.esa.int/img/2021/12/2022_Sentinels_digital_calendar_1024x768_211215$(echo $MNTH | sed s/^0// | sed s/^1$//).jpg"
+URL="https://esamultimedia.esa.int/img/2023/01/2023_Sentinels_GeneralWeb_1024x768_$(echo $MNTH | sed s/^0//).jpg"
 
 if [ ! -f $CACHE_FILE ]; then
-    until wget $URL -O $CACHE_FILE &> /dev/null
+    until wget $URL -O $CACHE_FILE > /dev/null
     do
 	echo "No success... Wait and retry"
         sleep 5
